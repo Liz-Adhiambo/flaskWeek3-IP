0cd13a42f90ff5b109d0a18416c0320a8b1b0962
Liz <adhiamboliz3@gmail.com>
2022-05-08 12:20:43 +0300

    Added dummy posts

--- pitchblog.py
+++ pitchblog.py
@@ -1,8 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Liz lizzy',
+        'title': ' Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 20, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': ' Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 21, 2022'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
